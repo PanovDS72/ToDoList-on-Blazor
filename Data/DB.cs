3044fb2aaa08765838d503384953b82867da7cc4
Denis <panovds72@gmail.com>
2020-09-30 17:12:10 +0500

    Add project files.

---
+++ Data/DB.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoList.Models;
+
+namespace ToDoList.Data
+{
+    public class DB : DbContext
+    {
+        public DB(DbContextOptions<DB> options) : base(options) { }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
---
+++ Data/Repository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoList.Models;
+
+namespace ToDoList.Data.Repository
+{
+    public interface IRepository
+    {
+        IEnumerable<TodoItem> GetAllItems();
+        void AddTodo(string todoName);
+        void ValueChanged(TodoItem changedItem);
+        void DeleteItem(int id);
+    }
+}
